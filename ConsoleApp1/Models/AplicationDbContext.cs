fe7aa110243a86fc323c058871fea0366dafa511
Angelo <angelo.rubiomer@ug.edu.ec>
2023-06-26 22:22:44 -0500

    Finish

--- ConsoleApp1/Models/AplicationDbContext.cs
+++ ConsoleApp1/Models/AplicationDbContext.cs
@@ -4,14 +4,16 @@
 namespace ConsoleApp1.Models {
     public class ApplicationDbContext : DbContext
     {
+        private DbSet<Usuario> usuarios;
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=localhost;Database=tallerBackendRubioMero;User Id=sa;Password=clave;");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=tallerBackendRubioMero;User Id=sa;Password=angelo;");
             }
         }
 
-        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Usuario> Usuarios { get => usuarios; set => usuarios = value; }
     }
 }
